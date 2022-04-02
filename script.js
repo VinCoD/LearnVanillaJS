4942b0f7eda1e147b6c80e3ea0d463fb874101ca
VinCoD <5895.2020@students.ku.ac.ke>
2022-04-02 18:24:32 +0300

    map method

---
+++ script.js
@@ -0,0 +1,10 @@
+let MyPets = [{name:"Meowsalot", species:"cat", age:2}, {name: "Barksalot", species: "dog", age: 3}, {name: "Purrsloud", species: "cat", age:8}];
+
+
+
+let pets = MyPets.map(NamPets);
+
+function NamPets(x){
+  return x.name
+}
+ console.log(pets);
